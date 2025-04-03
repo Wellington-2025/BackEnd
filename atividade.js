edea3a5d1a22038c164b5b9c944dfe5a958d6014
Wellington-2025 <wellingtonjardel37@gmail.com>
2025-04-03 17:47:51 -0400

    exercicio

---
+++ atividade.js
@@ -0,0 +1,12 @@
+let nome = "wellington"
+let idade = 16
+let text = "Meu java";
+console.log('ola meu nome e',nome, "e tenho" , idade, "anos" )
+console.log(nome.toUpperCase()); 
+
+let eu = 17
+console.log(typeof eu); // declara como number
+
+let altura = 1.67
+let peso = 54
+console.log ((peso /(altura * altura).toFixed(2)));
---
+++ aula-01.js
@@ -0,0 +1,34 @@
+let valor = 13.1234;
+let text = "Meu projeto java script";
+let ativo = true;
+
+console.log(valor.toFixed(2)); // Arredonda o valor para duas casas decimais:
+console.log("tipo:" + typeof valor );// converte para texto
+console.log(text.toUpperCase()); //converte o texxto para maiuscolas
+console.log("tipo:" + typeof text );// converte para texto
+console.log(ativo.toString()); // converte para texto
+console.log("tipo:" + typeof ativo ); // retorna o tipo da variavel
+
+
+let estados = ['Pernambuco' , 'rio de janeiro' , 'acre' , 'rondonia'];
+let LojaRoupa = [];
+LojaRoupa.push('jeans', 'renner');
+console.log(estados[3]);
+console.log("tamanho:" + estados.length);// concentraçao
+console.log(LojaRoupa[1]);
+console.log('total letras: ' + LojaRoupa[1].length);
+
+ let eu; // variavel n definida
+ console.log(eu);
+// console.log(eu.length); //TypeError: Cannot read properties of undefined (reading 'length')
+let idade;
+console.log(idade +1 ); // Nan - not a number
+
+ let nula = null;
+ // console.log(nula.length) error
+ console.log(nula +1);
+
+ const aula = "javaScript" //se tiver so assim da erro se tiver assim e em baixo ta certo
+ console.log(aula)
+ 
+ 
