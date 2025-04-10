907fff94c0aa049661da7c661d3e1732653ba37c
Wellington-2025 <wellingtonjardel37@gmail.com>
2025-04-10 17:29:06 -0400

    atividade 02

---
+++ atv.js
@@ -0,0 +1,20 @@
+let numero = 10 + 2;
+let subtraçao = 10 - 2;
+let mutiplicaçao = 10 * 2;
+let divisao = 10 / 2;
+console.log(numero)
+console.log(subtraçao)
+console.log(mutiplicaçao)
+console.log(divisao)
+
+let idade = 16
+let idade2 = 18
+
+let responda = idade >= idade2 ? "sim, é mais velho" : "não é mais velho"
+console.log(responda)
+
+let Idade = 10
+console.log(Idade >= 18 ? "sim, maior de idade" : "Nao, menor de idade")
+
+let podeser = 19
+console.log(podeser % 2 == 0 ? "par" : "impar")
